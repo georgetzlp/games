7a7b3acecd0b66cf13abe08132052eef3fb26e89
wathhr <72303904+wathhr@users.noreply.github.com>
2023-12-29 15:06:47 +0200

    fix: modal content

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { type SvelteComponent, onMount } from 'svelte';
+  import { onMount } from 'svelte';
   import { browser } from '$app/environment';
 
-  export let content: HTMLElement | SvelteComponent | string;
+  export let content: ConstructorOfATypedSvelteComponent | string;
   export let open = true;
   export let wrapper: HTMLElement | undefined = browser ? document.body : undefined;
 
@@ -28,7 +28,11 @@
   >
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M378.4 71.4c8.5-10.1 7.2-25.3-2.9-33.8s-25.3-7.2-33.8 2.9L192 218.7 42.4 40.6C33.9 30.4 18.7 29.1 8.6 37.6S-2.9 61.3 5.6 71.4L160.7 256 5.6 440.6c-8.5 10.2-7.2 25.3 2.9 33.8s25.3 7.2 33.8-2.9L192 293.3 341.6 471.4c8.5 10.1 23.7 11.5 33.8 2.9s11.5-23.7 2.9-33.8L223.3 256l155-184.6z"/></svg>
   </button>
-  {@html content}
+  {#if typeof content === 'string'}
+    {content}
+  {:else}
+    <svelte:component this={content} />
+  {/if}
 </dialog>
 
 <style>
